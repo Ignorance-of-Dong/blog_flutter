0758f5dd1d279f2621298078044743a91fbcf609
Ignorance-of-Dong <1054914660@qq.com>
2019-11-18 16:42:31 +0800

    '添加搜索模块'

--- lib/pages/sarch/search_screen.dart
+++ lib/pages/sarch/search_screen.dart
@@ -1,35 +1,62 @@
 import 'package:blog_flutter/routes/navigatorUtil.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
 import '../../model/model.dart';
-import 'package:flutter_effects/flutter_text_effect.dart';
-const sentences = [
-    "What is design?",
-    "Design is not just",
-    "what it looks like and feels like.",
-    "Design is how it works. \n- Steve Jobs",
-    "Older people",
-    "sit down and ask,",
-    "'What is it?'",
-    "but the boy asks,",
-    "What can I do with it?. \n- Steve Jobs",
-    "Swift",
-    "Objective-C",
-    "iPhone",
-    "iPad",
-    "Mac Mini",
-    "MacBook Pro",
-    "Mac Pro",
-    "爱老婆",
-    "老婆和女儿"
-  ];
+// import 'package:flutter_effects/flutter_text_effect.dart';
+import '../../widgets/Diffscaletext/diff_scale_text.dart';
+
+const sentencess = [
+  '所有漂亮的结果都是在行动中',
+  '收获的你不需要很厉害才能开始',
+  '但你需要开始才能变得很厉害',
+  '_____________________________',
+  '我越来越相信',
+  '创造美好的代价是',
+  '努力、失望以及毅力',
+  '首先是疼痛',
+  '然后才是欢乐。——梵高',
+  '_____________________________',
+  '敢于面对困境的人',
+  '生命因此坚强',
+  '敢于挑战逆境的人',
+  '生命因此茁壮',
+  '_____________________________',
+  '努力的最大动力',
+  '在于你可以选择你想要的生活',
+  '而不是被生活选择',
+  '弱者只有千难万难',
+  '而勇者则能披荆斩棘',
+  '愚者只有声声哀叹',
+  '智者却有千路万路',
+  '_____________________________',
+];
 class SearchScreen extends StatefulWidget {
 	@override
 	_SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  
+  int diffScaleNext = 0;
+  Timer countdownTimer;
+  countdown(){
+    Timer countdownTimer =  new Timer.periodic(new Duration(seconds: 2), (timer) {
+        setState(() {
+          diffScaleNext++;
+        });
+    });
+  }
+  @override
+  void initState() {
+    countdown();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    countdownTimer?.cancel();
+    countdownTimer = null;
+    super.dispose();
+  }
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
@@ -52,52 +79,20 @@ class _SearchScreenState extends State<SearchScreen> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: 200.0,
-            child: Center(
-              child: LineBorderText(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "Search to Page",
-                    style: TextStyle(fontSize: 20),
+            color: Colors.black,
+            child: InkResponse(
+              child: Center(
+                child: DiffScaleText(
+                  text: sentencess[diffScaleNext % sentencess.length],
+                  textStyle: TextStyle(
+                    fontSize: 17.0, 
+                    color: Colors.white,
                   ),
                 ),
-                autoAnim: true
               ),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 200.0,
-            child: Center(
-              child: LineBorderText(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "Search to Page",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-                autoAnim: true
-              ),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 200.0,
-            child: Center(
-              child: LineBorderText(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "Search to Page",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-                autoAnim: true
-              ),
+              onTap: (){
+                
+              },
             ),
           )
         ],
@@ -143,7 +138,6 @@ class SearchBarDelegate extends SearchDelegate<String>{
 	Widget buildSuggestions(BuildContext context) { //重写buildSuggestions【设置推荐】
   var activelist = Provider.of<CounterNotifier>(context).activelist;
 		dynamic suggestionList = query.isEmpty ? activelist : activelist.where((input) => input['title'].contains(query)).toList();
-    print(suggestionList);
     List<Widget> view = List();
 
     for(var i = 0; i < suggestionList.length; i++) {
